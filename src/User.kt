498974891b76db35a9ad50adfd7dc95b6fd4bfda
2alia <146340747+2alia@users.noreply.github.com>
2024-10-30 16:54:46 +0000

    done with part C

--- src/User.kt
+++ src/User.kt
@@ -1,11 +1,24 @@
-data class User(val username:String, var password:String, var isAdmin:Boolean = false, var contactDetails: String = "")
+open class User(val username:String, var password:String, var isAdmin:Boolean = false, var contactDetails: String = ""){
+
+     open fun Menu() {
+        println("Greetings")
+     }
+ }
 
 val users = mutableListOf<User>()
 
-fun signUp(){
+fun signUp():User{
     var username: String
     var password:String
+    var choice:String
     var contactDetails: String
+
+
+    do {
+        println("1 SignUp as Admin:")
+        println("2 SignUp as Regular User:")
+        choice = readLine().orEmpty()
+    } while (choice.isBlank())
 
     do {
         println("Enter your username:")
@@ -21,9 +34,18 @@ fun signUp(){
         contactDetails = readLine().orEmpty()
     } while (contactDetails.isBlank())
 
-    val newUser = User(username, password, isAdmin = false, contactDetails= contactDetails)
+    val newUser:User
+    if (choice.toInt()==1){
+
+        newUser = Admin(username, password, isAdmin = true, contactDetails= contactDetails)
+    }else{
+        newUser = User(username, password, isAdmin = false, contactDetails= contactDetails)
+    }
+
+
     users.add(newUser)
     println("User $username has successfully signed up!")
+    return newUser
 
 }
 
